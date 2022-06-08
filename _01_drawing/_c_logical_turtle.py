656c9099611082ed3ef28cdf37ecdc9011241a7b
bosaphire <bosaphire@gmail.com>
2022-06-08 11:58:53 -0700

    i worked on this

--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -13,20 +13,20 @@ def screen_clicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
-    # 7. Move the turtle to a new location using .goto(x, y)
-
+    jeff.penup()
+    # 7. Move the turtle to a new location using .goto( x, y)
+    jeff.goto(x=8, y=54)
 
 def turtle_clicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+    for i in range(3):
         # 9. Make the turtle spin 360 degrees using the .right() method
-        
+        jeff.right(985)
         # 10. Use the .color() method and getRandomColor() function to change
         # the color of the turtle,
-        # myTurtle.color(get_random_color())
+        jeff.color(get_random_color())
 
 
 if __name__ == '__main__':
@@ -34,16 +34,20 @@ if __name__ == '__main__':
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    jeff = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    jeff.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    jeff.color('green')
+    jeff.pencolor('red')
     # 4. Set and new width, length, and outline of our turtle
-    #    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+    jeff.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
 
     # 5. Uncomment the following line and replace 'my_turtle' with your turtle
-    # my_turtle.onclick(turtle_clicked)
+    jeff.onclick(turtle_clicked)
+
+
+
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -11,18 +11,22 @@ def get_random_color():
 
 if __name__ == '__main__':
     window = turtle.Screen()
-    window.bgcolor('white')
+    window.bgcolor('black')
     
     # Make a new turtle
-    
+    jeff = turtle.Turtle()
     # This code sets our shape to a turtle
-    
+    jeff.shape('turtle')
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
+    jeff.speed(0)
     # Set your turtle's color using .color('green')
-    
+    jeff.pencolor(get_random_color())
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(360):
+        jeff.color(get_random_color())
+        jeff.forward(10*i)
+        jeff.right(360/10)
+        jeff.turtlesize(stretch_wid=10+i)
         # Set the turtle color to a random color
 
         # Move the turtle (5*i) pixels. 'i' is the loop variable
